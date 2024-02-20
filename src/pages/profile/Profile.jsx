a0ea777780b45cffc09f3dde5942cfb277ca1687
alok-56 <alokkumar11746@gmail.com>
2024-02-20 13:18:03 +0530

    update api

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -5,7 +5,7 @@ import Home from "../../assets/home.png";
 import Product from "../../assets/products.png";
 import QR from "../../assets/profile-qr.png";
 import CreateQR from "../../assets/createQR.svg";
-import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowRight, MdUpdate } from "react-icons/md";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { FaFilter } from "react-icons/fa";
 import { MdFileDownload } from "react-icons/md";
@@ -23,7 +23,9 @@ import Help from "../../assets/help.svg";
 import { GetProfile } from "../../Api/Users";
 import { DeleteProfileQr } from "../../Utility/QrType/DeleteQr";
 import { useNavigate } from "react-router-dom";
+import View1 from "../../assets/edit.png";
 import { SpinnerRoundOutlined, SpinnerCircularFixed } from "spinners-react";
+import Modal from "../Modal/Modal";
 
 function Profile() {
   const [data, setData] = useState([]);
@@ -31,6 +33,19 @@ function Profile() {
   const navigate = useNavigate();
   const [load, setLoad] = useState(false);
   const [filterText, setFilterText] = useState("");
+  const [open, setOpen] = useState(false);
+  const [uniqueId, setUniqueId] = useState("");
+  const [type, setType] = useState("");
+
+  const OpnemOdal = (id, types) => {
+    setType(types);
+    setUniqueId(id);
+    setOpen(true);
+  };
+
+  const CloseModal = () => {
+    setOpen(false);
+  };
 
   const ChangeLoad = () => {
     setLoad(false);
@@ -110,6 +125,15 @@ function Profile() {
           }}
         />
       ) : null}
+
+      <Modal
+        isOpen={open}
+        closeModal={CloseModal}
+        id={uniqueId}
+        type={type}
+        ChangeLoad={ChangeLoad}
+        Profile={Profile}
+      ></Modal>
 
       <div className="sidebar">
         <div className="logo-image-container">
@@ -227,7 +251,7 @@ function Profile() {
                   borderRadius: 20,
                   height: 40,
                 }}
-              >
+              >c
                 <th className="cellStyleHeader">QR ID</th>
                 <th className="cellStyleHeaderCategory">Category</th>
                 <th className="cellStyleHeaderType">Data</th>
@@ -259,6 +283,13 @@ function Profile() {
                           textAlign: "center",
                         }}
                       >
+                        <img
+                          src={View1}
+                          alt=""
+                          style={{ width: 20, height: 20, padding: 0 }}
+                          onClick={() => OpnemOdal(item.UniqueId, item.Qrtype)}
+                        />
+
                         <img
                           src={View}
                           alt=""
